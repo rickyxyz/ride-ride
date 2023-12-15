a7998af5f7fd16b316488a20872db4789bcf0e2d
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-12-15 17:43:27 +0700

    deploy: test fix basepath

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   plugins: [react()],
-  base: mode === 'development' ? '/' : '/ride-ride/',
+  base: '/ride-ride/',
   build: {
     manifest: true,
     rollupOptions: {
